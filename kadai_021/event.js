bfdf16d61688bb9fee93e6d4b4681ad937e20238
K56az <icr56az@gmail.com>
2025-04-27 12:45:08 +0900

    Add files via upload

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,9 +1,20 @@
 // const btn=document.getElementById('btn');
 // const text=document.getElementById('text');
+// btn.addEventListener(setTimeout(()=>{'click',text.textContent='ボタンをクリックしました'},2000));
+
+
+// タイマーがボタンのクリックイベントの外にある
+// const btn=document.getElementById('btn');
+// const text=document.getElementById('text');
 // setTimeout(()=>{btn.addEventListener('click',text.textContent='ボタンをクリックしました')},2000);
+
+
+// タイマーがボタンのクリックイベントの外にある
+// const btn=document.getElementById('btn');
+// const text=document.getElementById('text');
+// btn.addEventListener('click',setTimeout(()=>{text.textContent='ボタンをクリックしました'},2000));
 
 
 const btn=document.getElementById('btn');
 const text=document.getElementById('text');
-btn.addEventListener('click',setTimeout(()=>{text.textContent='ボタンをクリックしました'},2000));
-
+btn.addEventListener('click',()=>{setTimeout(()=>{text.textContent='ボタンをクリックしました'},2000)});
